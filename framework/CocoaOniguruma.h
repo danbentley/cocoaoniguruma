1975e663e356522db5ac334e8e223e9c1bf29567
Dan Bentley <dan@numiko.com>
2016-05-13 11:59:21 +0100

    Adding umbrella header

---
+++ framework/CocoaOniguruma.h
@@ -0,0 +1,13 @@
+//
+//  CocoaOniguruma.h
+//  CocoaOniguruma
+//
+//  Created by Dan Bentley on 13/05/2016.
+//
+//
+
+#ifndef CocoaOniguruma_h
+#define CocoaOniguruma_h
+
+
+#endif /* CocoaOniguruma_h */
